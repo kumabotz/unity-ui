961c2fc14d8910e52964ba1c08758196f30236bb
Tery Lim <kumabotz@gmail.com>
2019-12-09 15:48:25 +1300

    Working with text

---
+++ Assets/Scripts/TextTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextTest : MonoBehaviour
+{
+    public string message;
+    private Text textInstance;
+
+    void Start()
+    {
+        textInstance = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        textInstance.text = message;
+    }
+}
