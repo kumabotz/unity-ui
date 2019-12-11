55a5e3d69e0a8da4a32ec58e4cc53142654268f6
Tery Lim <kumabotz@gmail.com>
2019-12-11 19:56:58 +1300

    Working with button

---
+++ Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log("On Click");
+    }
+
+    public void OnOver()
+    {
+        Debug.Log("On Over");
+    }
+
+    public void OnExit()
+    {
+        Debug.Log("On Exit");
+    }
+}
