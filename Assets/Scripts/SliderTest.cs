9bd7634147a96ec6ecd8822d031a116a22d1f094
Tery Lim <kumabotz@gmail.com>
2019-12-19 14:32:58 +1300

    Create a simple health bar

---
+++ Assets/Scripts/SliderTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderTest : MonoBehaviour
+{
+    public Slider sliderInstance;
+
+    void Start()
+    {
+        sliderInstance.minValue = 0;
+        sliderInstance.maxValue = 100;
+        sliderInstance.wholeNumbers = true;
+        sliderInstance.value = 50;
+    }
+
+    public void OnValueChanged(float value)
+    {
+        Debug.Log($"New value: {value}");
+    }
+}
