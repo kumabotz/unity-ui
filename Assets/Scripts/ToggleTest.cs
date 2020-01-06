a4804dea5eaa8a4ee6968bfbc9cbb43d29d6f910
Tery Lim <kumabotz@gmail.com>
2020-01-06 15:53:24 +1300

    Toggles, dropdowns and the input field

---
+++ Assets/Scripts/ToggleTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class ToggleTest : MonoBehaviour
+{
+    private ToggleGroup toggleGroupInstance;
+
+    public Toggle CurrentSelection => toggleGroupInstance.ActiveToggles().FirstOrDefault();
+
+    void Start()
+    {
+        toggleGroupInstance = GetComponent<ToggleGroup>();
+        Debug.Log($"First selected {CurrentSelection.name}");
+        SelectToggle(2);
+    }
+
+    public void SelectToggle(int index)
+    {
+        var toggles = toggleGroupInstance.GetComponentsInChildren<Toggle>();
+        toggles[index].isOn = true;
+    }
+}
